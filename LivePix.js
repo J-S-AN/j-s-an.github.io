fd50b715164feb2737d2cd42272b7cda5dba7ccf
JOSÉ STATUS YT <juniorgomesavils42@gmail.com>
2026-04-27 13:06:29 -0300

    Update LivePix.js

--- LivePix.js
+++ LivePix.js
@@ -1,38 +1,47 @@
-(function() {
+(function () {
 
-  function iniciarLivePix() {
+  function criarLivePix() {
     const iframe = document.createElement("iframe");
     iframe.id = "livepix-alert";
     iframe.src = "https://widget.livepix.gg/embed/47e1400a-fddf-43b3-ac88-ba0a902b814a?alerts=true";
     iframe.allow = "autoplay; fullscreen";
-
-    iframe.style.position = "fixed";
-    iframe.style.top = "0";
-    iframe.style.left = "0";
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
     iframe.style.border = "none";
     iframe.style.zIndex = "999999";
     iframe.style.pointerEvents = "none";
 
-    document.body.appendChild(iframe);
+    // Detecta celular
+    const mobile = window.innerWidth < 768;
 
-    // liberar som após interação (OBRIGATÓRIO mobile)
-    function liberarAudio() {
-      iframe.src = iframe.src;
-      document.removeEventListener("touchstart", liberarAudio);
-      document.removeEventListener("click", liberarAudio);
+    if (mobile) {
+      // 📱 MODO CELULAR (barra inferior)
+      iframe.style.position = "fixed";
+      iframe.style.left = "0";
+      iframe.style.bottom = "0";
+      iframe.style.width = "100%";
+      iframe.style.height = "120px";
+    } else {
+      // 💻 MODO PC (tela cheia)
+      iframe.style.position = "fixed";
+      iframe.style.top = "0";
+      iframe.style.left = "0";
+      iframe.style.width = "100vw";
+      iframe.style.height = "100vh";
     }
 
-    document.addEventListener("touchstart", liberarAudio, { passive:true });
+    document.body.appendChild(iframe);
+
+    // liberar áudio após interação (necessário)
+    function liberarAudio() {
+      iframe.src = iframe.src;
+      document.removeEventListener("click", liberarAudio);
+      document.removeEventListener("touchstart", liberarAudio);
+    }
+
     document.addEventListener("click", liberarAudio, { passive:true });
+    document.addEventListener("touchstart", liberarAudio, { passive:true });
   }
 
-  // Espera o site terminar de carregar (importante no mobile)
-  if (document.readyState === "complete") {
-    setTimeout(iniciarLivePix, 2000);
-  } else {
-    window.addEventListener("load", () => setTimeout(iniciarLivePix, 2000));
-  }
+  // carrega depois que o site terminar
+  window.addEventListener("load", () => setTimeout(criarLivePix, 2000));
 
 })();
